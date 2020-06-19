05c35c7e1922e228c57601eb3c85c465b6b033b4
fakeboyas <ovoivi9@gmail.com>
2020-06-19 15:00:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+import { penjumlahan, pembagian, perkalian } from "./soal2.js";
+
+let a = 10;
+let b = 5;
+
+console.log(penjumlahan(a, b));
+console.log(pembagian(a, b));
+console.log(perkalian(a, b));
---
+++ soal2.js
@@ -0,0 +1,16 @@
+function penjumlahan(a, b) {
+    let tambah = a + b;
+    return tambah;
+}
+
+function pembagian(a, b) {
+    let bagi = a / b;
+    return bagi;
+}
+
+function perkalian(a, b) {
+    let kali = a * b;
+    return kali;
+}
+
+export { penjumlahan, pembagian, perkalian };
